a8d7a9b4500b26f494630ee29a72db63a178ffee
Rysj0 <1161297560@qq.com>
2017-12-25 22:56:39 +0800

    第一部分代码完成

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SplashActivity.java
@@ -1,8 +1,48 @@
 package cn.edu.gdmec.android.boxuegu.activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-/**
- * Created by student on 17/12/25.
- */
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class SplashActivity {
+import cn.edu.gdmec.android.boxuegu.R;
+
+public class SplashActivity  extends AppCompatActivity {
+    private TextView tv_version;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//        设置此界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        init();
+
+    }
+    private void init() {
+        tv_version = (TextView) findViewById(R.id.tv_version);
+        try{
+//            获取程序管理信息
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(),0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+//        利用Timer让此界面延迟3秒在跳转，Timer中有一个线程，这个线程不断执行task
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+        timer.schedule(task,3000);//设置这个task在延迟3秒自动跳转
+    }
+
 }
