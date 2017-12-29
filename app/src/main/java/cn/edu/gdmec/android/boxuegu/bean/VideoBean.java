22d545a6bc054dae633180488333f7dd27ee8e15
lzk0725 <1004511748@qq.com>
2017-12-29 10:54:51 +0800

    课程模块  待续

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/bean/VideoBean.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.android.boxuegu.bean;
+
+/**
+ * Created by student on 17/12/29.
+ */
+
+public class VideoBean {
+    public int chapterId;//章节Id
+    public int videoId;//视频Id
+    public String videoPath;//视频播放地址
+    public String title;//章节标题
+    public String secondTitle;//视频标题
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/view/ViewPagerIndicator.java
@@ -0,0 +1,51 @@
+package cn.edu.gdmec.android.boxuegu.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import cn.edu.gdmec.android.boxuegu.R;
+
+
+/**
+ * Created by student on 17/12/29.
+ */
+
+public class ViewPagerIndicator extends LinearLayout {
+
+    private int mCount;//小圆点的个数
+    private int mIndex;//当前小圆点位置
+    private Context context;
+
+    public ViewPagerIndicator(Context context) {
+        super(context);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setGravity(Gravity.CENTER);
+    }
+    public void setCurrentPosition(int currentIndex){
+        mIndex = currentIndex;
+        removeAllViews();
+        int pex = 5;//内边距
+        for (int i=0;i<mCount;i++){
+            ImageView imageView = new ImageView(context);
+            if (mIndex == i){
+                //蓝色为选中
+                imageView.setImageResource(R.drawable.indicator_on);
+            }else {
+                //灰色图片
+                imageView.setImageResource(R.drawable.indicator_off);
+            }
+            imageView.setPadding(pex,0,pex,0);
+            addView(imageView);
+        }
+    }
+
+    public void setCount(int count){
+        this.mCount = count;
+    }
+}
